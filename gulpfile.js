b8bf5516492eadf8dfe264ad0b76919e591ddca0
kamarmack <kamarmack112@gmail.com>
2022-03-06 08:08:18 -0600

    moves proj to browser by adding browserify, tsify, vinyl-source-stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json');
-gulp.task('default', function () {
-  return tsProject
-    .src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const tsify = require('tsify');
+
+const paths = {
+  pages: ['src/*.html'],
+};
+gulp.task('copy-html', function () {
+  return gulp
+    .src(paths.pages)
+    .pipe(gulp.dest('dist'));
 });
+gulp.task('default',
+  gulp.series(
+    gulp.parallel('copy-html'),
+    function () {
+      return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {},
+      })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+    },
+  ),
+);
